366478ed749dd3ad11cd174779451b942c31168f
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2020-12-12 11:05:47 -0500

    finished HTML routes
    created 4 html routes
    index
    create user
    game info
    wish list

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,11 +9,24 @@ var path = require("path");
 // ROUTES
 
 module.exports = function(app) {
+    // INDEX HTML
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
-    // INDEX
+    // CREATE USER HTML
+    app.get("/create-user", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/createuser.html"));
+    });
 
-    // USER CREATOR
+    // GAME INFO HTML
+    app.get("/game-info", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/gameinfo.html"));
+    });
 
-    // 
+    // WISHLIST HTML
+    app.get("/wish-list", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/wishlist.html"));
+    });
     
-}
+};

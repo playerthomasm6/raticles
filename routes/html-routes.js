6e2b9f895c3dba29da1cb11cfda128b10cdebd5d
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2020-12-12 10:53:51 -0500

    updated API Routes and HTML routes
    started HTML routes and API Routes

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+//  _________________________
+// |                         |
+// |        HTML ROUTES      |
+// |_________________________|
+
+// DEPENDENCIES
+var path = require("path");
+
+// ROUTES
+
+module.exports = function(app) {
+
+    // INDEX
+
+    // USER CREATOR
+
+    // 
+    
+}

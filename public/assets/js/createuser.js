d95014f388949d890fe9404783d4e7a7774a1bcf
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2020-12-15 20:49:02 -0500

    debug create user.js
    tried to fix on click form submit

--- public/assets/js/createuser.js
+++ public/assets/js/createuser.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
     // Getting references to our form and input
-    var signUpForm = $("submitBtn");
-    var firstInput = $("Fname");
-    var secondInput = $("Lname");
+    var signUpForm = $("#submitBtn");
+    var firstInput = $("#Fname");
+    var secondInput = $("#Lname");
     var uName = $("#Username");
     var emailInput = $("#exampleInputEmail1");
     var passwordInput = $("#exampleInputPassword1");
   
     // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", function(event) {
+    signUpForm.on("click", function(event) {
       event.preventDefault();
       var userData = {
-        firstname: firstName.val().trim(),
-        secondname: secondName.val().trim(),
-        username: userName.val().trim(),
+        firstname: firstInput.val().trim(),
+        secondname: secondInput.val().trim(),
+        username: uName.val().trim(),
         email: emailInput.val().trim(),
         password: passwordInput.val().trim()
       };
+      console.log("This submit worked Ln 20 createuser.js" + userdata)
   
       if (!userData.email || !userData.password) {
         return;
@@ -25,7 +26,7 @@ $(document).ready(function() {
       signUpUser(userData.firstname, userData.secondname,userData.username,userData.email, userData.password);
       firstInput.val("");
       secondInput.val("");
-      userName.val("");
+      uName.val("");
       emailInput.val("");
       passwordInput.val("");
     });
@@ -34,11 +35,11 @@ $(document).ready(function() {
     // Otherwise we log any errors
     function signUpUser(firstname, secondname, username, email, password) {
       $.post("/api/signup", {
-        firstname: firstname,
-        secondname: secondname,
-        username: username,
-        email: email,
-        password: password
+        Firstname: userData.firstname,
+        Lastname: userData.secondname,
+        Username: userData.uName,
+        Email: userData.email,
+        Password: userData.passwordInput
       })
         .then(function(data) {
           window.location.replace("/members");

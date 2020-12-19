3308487780bd62f9eaa27d4e971c4a78bd4add82
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2020-12-19 09:59:23 -0500

    made some changes that will be overwritten by main

--- public/assets/js/igdb.js
+++ public/assets/js/igdb.js
@@ -1,26 +1,51 @@
 // const { json } = require("sequelize/types");
-module.exports = 
-
 function searchGame() {
 
-var searchGames = $('#searchGames').val()
+var searchGames = $('#search-input').val()
 searchGamesString = JSON.stringify(searchGames)
 console.log(searchGames)
+//GET https://<your-request-url>/games/?search=zelda&fields=id,name
 
 var settings = {
   "url": "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games/",
   "method": "POST",
+  "data": 'search' + searchGamesString + '; fields name, age_ratings;', 
   "timeout": 0,
   "headers": {
     "Client-ID": "wtw0hnai6i7njmhspijabmmom6yyh5",
     "Authorization": "Bearer jpkrnm1ejzzqfhl1c2wu2wtfb11w2w",
-    "Content-Type": "text/plain",
+    "Accept": "application/json",
     // "Cookie": "__cfduid=d95ef25d3998aca8e5108fbbfb75328e11608079973"
   },
-  "data": 'search' + searchGamesString + '; fields name,age_ratings,aggregated_rating,artworks,franchise,first_release_date,follows,franchises,hypes,involved_companies,parent_game,platforms,rating,release_dates,storyline,summary,videos;',
 };
 
 $.ajax(settings).done(function (response) {
+  console.log(response[0].age_ratings[0])
   console.log(response);
+
+
+  
+  let newElem1 = ("#search-results");
+  let gameResponse = `<p class="game-result"> ${response[0].name} and this is the platform ${response[0].platform} </p>`
+  console.log(gameResponse);
+  $(newElem1).append(gameResponse);
+  
 });
 }
+
+$("#search-time").on("click", function(event) {
+  event.preventDefault();
+  searchGame()
+});
+
+function gameTitlePoster(response) {
+  for (i =0; i < response.length; i++) {
+    let newElem1 = ("#search-results");
+  let gameResponse = `<p class="game-result"> ${response[i].storyline}</p>`
+  console.log(gameResponse);
+  $(newElem1).append(gameResponse);
+  }
+}
+
+
+//740?fields=id

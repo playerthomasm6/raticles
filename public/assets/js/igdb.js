5ced9695b5b30570c72364f9e4adcb8e2e7625ab
Matt Harris <711mattman@gmail.com>
2020-12-20 12:26:12 -0500

    Some changes didnt come in when I pulled from origin. So I had to add them in by hand

--- public/assets/js/igdb.js
+++ public/assets/js/igdb.js
@@ -89,3 +89,72 @@ function addLibrary(gameName) {
 
   });
 }
+
+function addWishList(gameName) {
+  console.log("you added a game to with list")
+};
+
+function getGameInfo(gameName) {
+  console.log("you want more information")
+  var searchGames = gameName
+  searchGamesString = JSON.stringify(searchGames)
+  console.log(searchGames)
+  //GET https://<your-request-url>/games/?search=zelda&fields=id,name
+
+  var settings = {
+    "url": "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games/",
+    "method": "POST",
+    "data": `fields name, genres.name, age_ratings.*, artworks.*, cover.*, platforms.*; search  ${searchGamesString};`,
+    "timeout": 0,
+    "headers": {
+      "Client-ID": "wtw0hnai6i7njmhspijabmmom6yyh5",
+      "Authorization": "Bearer jpkrnm1ejzzqfhl1c2wu2wtfb11w2w",
+      "Accept": "application/json",
+      // "Cookie": "__cfduid=d95ef25d3998aca8e5108fbbfb75328e11608079973"
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response)
+    
+    console.log("wow look at all this info" + response[0])
+
+  });
+};
+
+
+// On Click for search button
+$("#search-time").on("click", function (event) {
+  event.preventDefault();
+  
+  searchGame();
+});
+
+// LIBRARY
+// On click event to add game to library
+$(document).on("click", "button.addLibrary", function (event) {
+  console.log($(this).data('a'));
+  var gameName = $(this).data('a').toLowerCase();
+  console.log(gameName);
+  addLibrary(gameName);
+});
+
+
+// WISHLIST
+// On click event to add game to wishlist
+$(document).on("click", "button.addWishList", function (event) {
+  console.log($(this).data('a'));
+  var gameName = $(this).data('a').toLowerCase();
+  console.log(gameName);
+  addWishList(gameName);
+});
+
+
+// INFO
+// On click event to get more game info
+$(document).on("click", "button.moreInfo", function (event) {
+  console.log($(this).data('a'));
+  var gameName = $(this).data('a').toLowerCase();
+  console.log(gameName);
+  getGameInfo(gameName);
+});
--- server.js
+++ server.js
@@ -35,7 +35,7 @@ require("./routes/api-routes.js")(app);
 require("./routes/User-api-routes.js")(app); // example routes
 require("./routes/Wish-api-routes.js")(app);
 require("./routes/Library-api-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================

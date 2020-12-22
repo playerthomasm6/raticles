983e306de200f674c68879ddee902043b38a8543
Hisham Kader <hisham319@hotmail.com>
2020-12-21 21:22:43 -0500

    add new code to gameinfo.js

--- public/assets/js/gameinfo.js
+++ public/assets/js/gameinfo.js
@@ -1,2 +1,21 @@
+$(document).ready(function () {
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/game-info").then(function (data) {
+    
+    
+    let gameinfo = ``
+    
+    
+    
+    });
 
 
+
+
+
+
+
+
+
+})
